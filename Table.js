19172a869fb6c9f106246d342ff4588b55ac39ce
Shibinraja <59076150+Shibinraja@users.noreply.github.com>
2020-03-02 09:43:28 +0530

    All files

---
+++ Table.js
@@ -0,0 +1,36 @@
+let add = document.getElementById("btn-add");
+let Table = document.getElementById("tb");
+
+
+add.addEventListener("click", function(){
+Length = Table.rows.length; 
+Trow = Table.insertRow(Table.rows.length);
+Tno = Trow.insertCell(0);
+Taction = Trow.insertCell(1);
+Tno.innerHTML = ++Length;
+Trow.setAttribute("id" , Length)
+Button = document.createElement("button");
+Button.innerHTML = "Delete"
+Button.setAttribute("class" , "btn btn-danger");
+Button.setAttribute("onclick" , "deleted(this,this.parentNode.parentNode.id)");
+Taction.appendChild(Button);
+Trow.appendChild(Tno);
+Trow.appendChild(Taction);
+Table.appendChild(Trow);
+
+})
+
+function deleted(act,id){
+    console.log(++id);
+    var row = act.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+    Tlength = Table.rows.length;
+    for(var i = id ; i <= Tlength+1 ; i++){
+
+        Tno = document.getElementById(i).cells[0]
+        Tno.innerHTML = i-1;
+    }
+
+
+
+}
